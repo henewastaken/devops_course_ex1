91946b0e20b0e2eda2cf80442a9c12c620bfea0e
Petra <hene.andersson@gmail.com>
2024-10-04 14:38:28 +0300

    Ex1

---
+++ Service1/app.py
@@ -0,0 +1,47 @@
+import subprocess
+import socket
+import psutil
+import requests
+from flask import Flask, jsonify
+
+app = Flask(__name__)
+
+def get_ip():
+    return socket.gethostbyname(socket.gethostname())
+
+def get_running_processes():
+    return subprocess.getoutput("ps -ax")
+
+def get_disk_space():
+    return psutil.disk_usage('/').free
+
+
+def get_uptime():
+    return psutil.boot_time()
+
+@app.route('/')
+def service1_info():
+    service2_url = 'http://service2:8200'
+    try:
+        service2_response = requests.get(service2_url)
+        service2_info = service2_response.json()
+    except requests.exceptions.RequestException as e:
+        service2_info = {'error': 'Unable to reach Service2', 'details': str(e)}
+
+
+    # Collect Service1 info
+    service1_info = {
+        'service': 'Service1',
+        'ip_address': get_ip(),
+        'running_processes': get_running_processes(),
+        'free_disk_space': get_disk_space(),
+        'uptime': get_uptime(),
+    }
+
+    return jsonify({
+        'service1': service1_info,
+        'service2': service2_info
+    })
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=8199)
---
+++ Service2/app.js
@@ -0,0 +1,43 @@
+const express = require('express');
+const os = require('os');
+const osu = require('node-os-utils');
+const { execSync } = require('child_process');
+const app = express();
+const port = 8200;
+
+function getIP() {
+  const interfaces = os.networkInterfaces();
+  for (const name of Object.keys(interfaces)) {
+    for (const intf of interfaces[name]) {
+      if (intf.family === 'IPv4' && !intf.internal) {
+        return intf.address;
+      }
+    }
+  }
+}
+
+function getRunningProcesses() {
+  return execSync('ps -ax').toString();
+}
+
+function getFreeDiskSpace() {
+  return execSync('df -h /').toString();
+}
+
+function getUptime() {
+  return os.uptime();
+}
+
+app.get('/', (req, res) => {
+  res.json({
+    service: 'Service2',
+    ip_address: getIP(),
+    running_processes: getRunningProcesses(),
+    free_disk_space: getFreeDiskSpace(),
+    uptime: getUptime(),
+  });
+});
+
+app.listen(port, () => {
+  console.log(`Service2 listening at http://0.0.0.0:${port}`);
+});
